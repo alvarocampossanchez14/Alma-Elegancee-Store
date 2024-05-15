18610bc63c74842cf45413632491b51023ce48a0
alvarocampossanchez14 <alvarocamposdev@gmail.com>
2024-05-16 00:52:26 +0200

    Add home route button

--- src/pages/Proximamente.js
+++ src/pages/Proximamente.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Proximamente = () => {
     return (
         <div className="flex flex-col justify-center items-center h-screen gap-5">
             <h1 className="text-5xl">Proximamente...</h1>
+            <button><Link to="/home">Ir al inicio de la web (BETA)</Link></button>
         </div>
     );
 }
